42af9c53413dae916146e11672ff643996481460
MarwanZaouiisbest <marwanzaoui130913@icloud.com>
2026-03-13 16:06:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+fetch("https://api.alquran.cloud/v1/surah/1/ar.alafasy")
+.then(response => response.json())
+.then(data => {
 
+const versets = data.data.ayahs
+const container = document.getElementById("versets")
+
+versets.forEach(v => {
+
+const p = document.createElement("p")
+p.className="arabe"
+p.textContent = v.text
+
+container.appendChild(p)
+
+})
+})
