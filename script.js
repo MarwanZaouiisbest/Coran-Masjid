4b5561824e8cbe61f17d2333f446ca6aa80c839e
MarwanZaouiisbest <marwanzaoui130913@icloud.com>
2026-03-20 17:50:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ arabe.forEach((v,i)=>{
 const div=document.createElement("div")
 div.className="verset"
 
+// texte arabe
 const a=document.createElement("p")
 a.className="arabe"
 a.innerText=v.text
 
+// traduction
 const f=document.createElement("p")
 f.className="fr"
 f.innerText=francais[i].text
 
+// 🔊 AUDIO
 const audio=document.createElement("audio")
-audio.controls=true
-audio.src="https://cdn.islamic.network/quran/audio/128/ar.alafasy/"+v.number+".mp3"
+audio.controls = true
+audio.src = "https://cdn.islamic.network/quran/audio/128/ar.alafasy/" + v.number + ".mp3"
 
+// ajouter tout
 div.appendChild(a)
 div.appendChild(f)
 div.appendChild(audio)
